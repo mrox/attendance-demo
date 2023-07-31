f3efe5b0ff057e017768a9beac27fe3657c3bd6e
tuyentran <tuyen.appdev@gmail.com>
2023-07-31 16:53:14 +0700

    v

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+
 const app = express()
 const port = 3000
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('*', (req, res) => {
+app.post('*', (req, res) => {
   
   console.log(req.body);
   console.log(req.params);
